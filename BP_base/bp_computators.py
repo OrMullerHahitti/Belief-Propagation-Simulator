dcf1c371d71f28f6a34dd0cb6343e2be94b44963
Or Muller <ormullerh@gmail.com>
2025-03-17 21:24:16 +0200

    Add message handling and computation classes for belief propagation

--- BP_base/bp_computators.py
+++ BP_base/bp_computators.py
@@ -10,8 +10,11 @@ class MinSumComputator(BPComputator):
             result += message.message
 
         return result
-
-    def compute_R(self,cost_table:np.ndarray,messages:Message)->[Message]:
-        pass
+    #compute R as in min sum algorithm summing the cost table and the incoming messages and then computing the min
+    def compute_R(self,cost_table:np.ndarray,message:f)->Message:
+        cost_table = np.array(cost_table)
+        result = np.zeros_like(cost_table)
+        min_sum = np.min(cost_table + result, axis=0)
+        return Message(min_sum, message.recipient, message.sender)
     def get_belief(self):
         pass
