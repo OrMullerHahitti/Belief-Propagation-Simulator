efc7f4cc412723811aead560c3d1ae376c0c633e
Or Muller <ormullerh@gmail.com>
2025-03-20 15:24:35 +0200

    Refactor message handling and computation classes for belief propagation

--- BP_base/agents.py
+++ BP_base/agents.py
@@ -4,35 +4,12 @@ from abc import ABC,abstractmethod
 from typing import Dict, List, TypeAlias
 import numpy as np
 
-from DCOP_base import Agent, Computator
+from BP_base.components import Message, BPComputator
+from DCOP_base import Agent
 from saved_for_later.decorators import validate_message_direction
 from utils.randomes import create_random_table
 Iteration:TypeAlias = Dict[int,List['Message']]
-class Message():
-    def __init__(self,message:np.ndarray,sender:Agent,recipient:Agent):
-        self.message = message
-        self.sender = sender
-        self.recipient = recipient
-    def __hash__(self):
-        return hash((self.sender,self.recipient))
-    def __eq__(self, other):
-        return self.sender == other.sender and self.recipient == other.recipient
-    def __ne__(self, other):
-        return not self == other
-    def __str__(self):
-        return f"Message from {self.sender.name} to {self.recipient.name}: {self.message}"
-    def __repr__(self):
-        return self.__str__()
 
-class BPComputator(ABC):
-    @abstractmethod
-    def compute_Q(self,messages:List[Message]) -> List[Message]:
-        pass
-    @abstractmethod
-    def compute_R(self,cost_table:np.ndarray,messages:Message)->Message:
-        '''input: cost_table: np.ndarray, messages: List[Message]
-        output: List of messages computed from the cost table and the incoming messages for each variable node'''
-        pass
 
 
 
