94f27d546d0cbff9326dc51253992f2a9b7f15c5
Or Muller <ormullerh@gmail.com>
2025-04-10 19:04:54 +0300

    started working on the engine to run the inference

--- bp_base/bp_engine.py
+++ bp_base/bp_engine.py
@@ -4,27 +4,53 @@ import numpy as np
 
 from bp_base.agents import BPAgent
 from bp_base.components import Message
-from bp_variations.factor_graph import FactorGraph
+from bp_base.factor_graph import FactorGraph
 
 class BeliefPropagation(ABC):
     """
     Abstract engine for belief propagation.
     """
     def __init__(self, factor_graph: FactorGraph):
-        self.factor_graph = factor_graph
-        self.history = Dict[int, List[BPAgent]]  # Store history of beliefs
+        self.graph = factor_graph
+        self.iterations = Dict[int, List[BPAgent]]  # Store history of beliefs
 
-    @abstractmethod
-    def run_inference(self, max_iters: int = 10):
-        pass
+    def step(self):
+        """Run the factor graph algorithm."""
 
-    @abstractmethod
+        # compute messages to send and put them in the mailbox
+        for agent in self.graph.G.nodes():
+            agent.messages_to_send = agent.compute_messages(agent.mailbox)
+            agent.empty_mailbox()
+        # send the messages to the right nodes
+        for agent in self.graph.G.nodes():
+            for message in agent.messages_to_send:
+                message.sender.send_message(message.recipient, message)
+
+    def cycle(self):
+        for i in range(self.graph.G.diameter):
+            self.step()
+
+    def run(self, max_iter: int = 1000) -> None:
+        """
+        Run the factor graph algorithm for a maximum number of iterations.
+        :param max_iter: Maximum number of iterations to run.
+        """
+        for i in range(max_iter):
+            self.cycle()
+            if self.is_converged():
+                break
+
     def get_beliefs(self) -> Dict[str, np.ndarray]:
         ''' Return the beliefs of the factor graph.
         :return:
         :param: A dictionary mapping variable names to belief vectors.'''
         pass
 
-    @abstractmethod
+
     def get_map_estimate(self) -> Dict[str, int]:
         pass
+    def is_converged(self) -> bool:
+        if self.cycle[-1] == self.cycle[-2]:
+            return True
+        else:
+            return False
