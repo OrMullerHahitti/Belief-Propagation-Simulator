9a8b278c09624fcf8e79e064043a5757752b1e76
Or Muller <ormullerh@gmail.com>
2025-04-18 12:50:56 +0300

    add BPAgent, Message, and Computator protocol definitions to typing.py Delete types.py merge to one module

--- bp_base/types.py
+++
@@ -1,8 +0,0 @@
-from typing import TypeVar, Protocol
-
-class BPAgent(Protocol):
-    """Base protocol type for Belief Propagation agents"""
-    name: str
-    domain: int
-    mailbox: dict
-#TODO : add more protocols!
