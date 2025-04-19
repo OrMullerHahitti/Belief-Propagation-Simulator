c01510b9054882664e1a86fe11f01c9a11f6c564
Or Muller <ormullerh@gmail.com>
2025-04-19 19:06:08 +0300

    Refactor agents to remove computator parameter and update imports in main and config files config works for factor graphs TODO: add factor graph function to update computator for all agents in the graph

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from utils.create_factor_graphs_from_config import FactorGraphBuilder, get_project_root
-
+from utils.create_factor_graph_config import ConfigCreator, GraphConfig
 if __name__ == "__main__":
     # 1. Create a FactorGraphBuilder instance
     builder = FactorGraphBuilder()
 
     # 2. Build and save a factor graph from a config file
-    cfg_path = f"{get_project_root()}\\configs/factor_graph_configs/configs/factor_graph_configs/max-sum-cycle-3-random_intlow2,high100.pkl"
+    cfg_path = f"{get_project_root()}\\configs/factor_graph_configs/cycle-3-random_intlow2,high100.pkl"
     out_path = builder.build_and_save(cfg_path)
 
     print(f"Factor graph saved to: {out_path}")
