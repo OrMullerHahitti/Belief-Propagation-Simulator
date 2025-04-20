ba88aed031b457e3059d89e7c5c3fd0b5f65048f
Or Muller <ormullerh@gmail.com>
2025-04-20 21:31:51 +0300

    Add configuration creation and factor graph building modules
    doesnt completley work, working on it now for it to work from every place so route doesnt matter
    
    commiting so i can delete unrelated data from main

--- DCOP_base.py
+++ DCOP_base.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Callable, Set, TypeAlias
 import numpy as np
 
-from bp_base.components import Message
+from bp_base.typing import Message
 from utils.randomes import create_random_table
 from networkx import Graph
 class Computator(ABC):
