35be341bb654bbb3c6f8816b4e60a9bc5ee6d22c
Or Muller <ormullerh@gmail.com>
2025-05-04 16:03:29 +0300

    REFactor logging setup and improve file handling in Logger class

--- configs/loggers.py
+++ configs/loggers.py
@@ -9,32 +9,50 @@ from pathlib import Path
 import colorlog  # pip install colorlog
 import pytest
 
+from utils.path_utils import find_project_root
+
 # Create logs directory if it doesn't exist
-log_dir = 'test_logs'
+log_dir = find_project_root() / 'logs'
 os.makedirs(log_dir, exist_ok=True)
 
 # Set up root logger
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.INFO)
 
-# Clear any existing handlers
-if root_logger.handlers:
-    root_logger.handlers.clear()
+
 
 # Create console handler with colored formatting
 console_handler = colorlog.StreamHandler(sys.stdout)
-console_handler.setFormatter(colorlog.ColoredFormatter(
-    '%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    log_colors={
-        'DEBUG': 'cyan',
-        'INFO': 'green',
-        'WARNING': 'yellow',
-        'ERROR': 'red',
-        'CRITICAL': 'red,bg_white',
-    }
-))
 
 file_handler = logging.FileHandler(os.path.join(log_dir, 'debug_graph.log'))
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 root_logger.addHandler(file_handler)
+
+class Logger(logging.Logger):
+    """
+    Custom logger class to add a file handler for logging.
+    """
+    def __init__(self, name, level=logging.INFO,file=False):
+        super().__init__(name, level)
+
+        if file:
+            self.file_handler = logging.FileHandler(os.path.join(log_dir, f'{name}.log'))
+            self.file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+            self.addHandler(self.file_handler)
+            self.propagate = False
+
+        self.console =colorlog.StreamHandler(sys.stdout)
+        self.console.setFormatter(colorlog.ColoredFormatter(
+            '%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red,bg_white',
+            }
+        ))
+        self.addHandler(self.console)
+
+
 
