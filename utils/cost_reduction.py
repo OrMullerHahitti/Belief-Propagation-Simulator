aa0f6189c259f1f95c86628ebd5336132ef961d5
Or Muller <ormullerh@gmail.com>
2025-05-11 18:16:48 +0300

    Add modular engines for damping, splitting, and cost reduction
    Enhanced `BPEngine` with new modular classes: `DampingEngine`, `SplitEngine`, and others for more flexible processing. Introduced utility functions for specific operations and adjusted tests accordingly.

---
+++ utils/cost_reduction.py
@@ -0,0 +1,9 @@
+from bp_base.agents import FactorAgent, VariableAgent
+from bp_base.components import Message
+from typing import Tuple, List, Iterable
+
+
+def cost_reduction_all_factors(fac_a:Iterable[FactorAgent],x:float):
+    for factor in fac_a:
+            if factor.cost_table is not None:
+                factor.cost_table=factor.cost_table*x
