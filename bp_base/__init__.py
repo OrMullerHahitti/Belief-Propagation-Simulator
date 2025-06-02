0479916a82f0c22a912c8c1b18889de6c7420440
Or Muller <ormullerh@gmail.com>
2025-06-03 02:04:23 +0300

    feat: optimize message computation in belief propagation; improve logging and caching mechanisms

--- bp_base/__init__.py
+++ bp_base/__init__.py
@@ -1,1 +1,11 @@
+import logging
+import os
+
+log_level = os.environ.get('BP_LOG_LEVEL', 'CRITICAL').upper()
+logging.getLogger().setLevel(getattr(logging, log_level, logging.CRITICAL))
+
+for module_name in ['bp_base', 'utils', 'policies']:
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.CRITICAL)
+    logger.propagate = False
 # This file makes bp_base a Python package
