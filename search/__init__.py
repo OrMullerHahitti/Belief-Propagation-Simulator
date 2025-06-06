1c0e9db299e26928e7dc5479982404bcd7a3b033
Or Muller <ormullerh@gmail.com>
2025-06-06 13:00:37 +0300

    feat: add search module with DSA and MGM algorithms; implement search computator and engine

---
+++ search/__init__.py
@@ -0,0 +1,9 @@
+"""
+Search module for implementing search-based algorithms like DSA and MGM.
+This module extends the belief propagation framework to support search algorithms.
+"""
+
+from search.search_computator import SearchComputator
+from search.search_engine import SearchEngine
+
+__all__ = ['SearchComputator', 'SearchEngine']
