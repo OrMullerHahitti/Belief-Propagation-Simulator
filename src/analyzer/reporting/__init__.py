592551ea8a0d30105c569c83f30283a914a69e32
Or Muller <ormullera@gmail.com>
2025-10-08 17:12:20 -0400

    feat(reporting): add snapshot parser

--- src/analyzer/reporting/__init__.py
+++ src/analyzer/reporting/__init__.py
@@ -1,13 +1,17 @@
 """Modular snapshot analytics suited for report generation and future tooling."""
-# TODO: Implement missing analyzer.py and snapshot_parser.py modules
-# from .analyzer import AnalysisReport, SnapshotAnalyzer
-# from .snapshot_parser import MessageRecord, SnapshotRecord, load_snapshots, parse_snapshots
+
+from .snapshot_parser import (
+    MessageRecord,
+    SnapshotRecord,
+    load_snapshots,
+    parse_snapshots,
+    from_engine_snapshot_manager,
+)
 
 __all__ = [
-    # "AnalysisReport",
-    # "SnapshotAnalyzer",
-    # "MessageRecord",
-    # "SnapshotRecord",
-    # "parse_snapshots",
-    # "load_snapshots",
+    "MessageRecord",
+    "SnapshotRecord",
+    "parse_snapshots",
+    "load_snapshots",
+    "from_engine_snapshot_manager",
 ]
